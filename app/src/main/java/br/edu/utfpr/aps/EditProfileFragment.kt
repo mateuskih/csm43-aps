48b9f718084379f37a8fde7a5d5004187d0f9b02
Murilo <bc.murilo.mbc@gmail.com>
2024-08-22 01:08:28 -0300

    Upgrade Fragments

--- app/src/main/java/br/edu/utfpr/aps/EditProfileFragment.kt
+++ app/src/main/java/br/edu/utfpr/aps/EditProfileFragment.kt
@@ -1,4 +1,25 @@
 package br.edu.utfpr.aps
 
-class EditProfileFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class EditProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
